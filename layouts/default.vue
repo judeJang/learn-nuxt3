338cc07865bea37e521015d6eb965debde0136a1
mustit-hyjang <94949361+mustit-hyjang@users.noreply.github.com>
2024-04-09 14:36:14 +0900

    Nuxt link change navigateTo

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,9 +11,7 @@
           <q-btn stretch flat label="About" no-caps @click="navigate" />
         </NuxtLink>
         <q-separator dark vertical />
-        <NuxtLink v-slot="{ navigate }" custom to="/about">
-          <q-btn stretch flat label="Youtube" no-caps @click="moveYoutube()" />
-        </NuxtLink>
+        <q-btn stretch flat label="Youtube" no-caps @click="moveYoutube()" />
         <q-separator dark vertical />
         <NuxtLink v-slot="{ navigate }" custom to="/admin">
           <q-btn stretch flat label="admin" no-caps @click="navigate" />
