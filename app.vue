8384c031b8cdb85d526e06247efac1d0565d31b6
mustit-hyjang <94949361+mustit-hyjang@users.noreply.github.com>
2024-04-09 15:05:44 +0900

    nuxt runtime app context

--- app.vue
+++ app.vue
@@ -5,6 +5,17 @@
   </NuxtLayout>
 </template>
 <script lang="ts" setup>
-import type { LayoutKey } from '#build/types/layouts';
-const layout: LayoutKey = 'custom';
+// import type { LayoutKey } from '#build/types/layouts';
+// const layout: LayoutKey = 'custom';
+
+const nuxtApp = useNuxtApp();
+console.log('nuxtApp', nuxtApp);
+
+// vue application instance
+// nuxtApp.vueApp.component();
+// nuxtApp.vueApp.directive();
+// nuxtApp.vueApp.use();
+
+// provide 활용
+// nuxtApp.provide('hello', (msg: string) => `Hellow ${msg}`);
 </script>
